cd310f53cdd7908739149f1a4af99faac81eef02
Iva Kalandadze <kalandadzeiva33@gmail.com>
2023-02-07 21:47:38 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Info from './pages/Info';
+import Experience from './pages/Experience';
+import Education from './pages/Education';
+import Resume from './pages/Resume';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/info' element={<Info />}/>
+        <Route path='/expereince' element={<Experience />}/>
+        <Route path='/education' element={<Education />}/>
+        <Route path='/resume' element={<Resume />} />
+      </Routes>
     </div>
   );
 }
